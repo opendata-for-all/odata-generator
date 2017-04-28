38b3d3821de3e878df85c4bf953f6c540694bf23
hamza <hamza.eddouibi@gmail.com>
2017-04-28 16:00:06 +0200

    update generation results

--- examples/som.odata.generator.example/src-gen/schema.sql
+++ examples/som.odata.generator.example/src-gen/schema.sql
@@ -1,16 +1,20 @@
 
 # Create tables
 CREATE TABLE product(
-id INT,
-name VARCHAR(255),
-description VARCHAR(255),
-releaseddate DATE,
-discountinueddate DATE,
-rating INT,
-price DECIMAL,
+	id INT not null,
+	name VARCHAR(255) ,
+	description VARCHAR(255) ,
+	releaseddate DATE ,
+	discountinueddate DATE ,
+	rating INT ,
+	price DECIMAL ,
+	supplier_id INT,
+	PRIMARY KEY (id)
 );
 CREATE TABLE supplier(
-id INT,
-name VARCHAR(255),
-
+	id INT not null,
+	name VARCHAR(255) ,
+	
+	
+	PRIMARY KEY (id)
 );

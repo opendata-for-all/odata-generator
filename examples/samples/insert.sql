a64a331ab0ff8f8c06589d602ebbc6150f21b70f
hamzaed <hamza.eddouibi@gmail.com>
2017-05-06 14:37:54 +0200

    add sample data

---
+++ examples/samples/insert.sql
@@ -0,0 +1,10 @@
+USE `odata`;
+
+INSERT INTO `address` VALUES (1,'Spain','Joaquin Costa','Barcelona','Barcelona','08001'),(2,'France','Maréchal Joffre','Nantes','Loire atlantique','44000');
+
+INSERT INTO `supplier` VALUES (1,'EcoLife',1),(2,'GrandBio',2);
+
+INSERT INTO `product` VALUES (1,'Milk','Bio milk','2016-06-09','2017-04-29',5,3,1),(2,'Fromage','Fromage bio','2017-05-10',NULL,4,5,2);
+
+
+
